daade9e7b56ff008e2722828609cad00c2b234c1
zerui_liu <liuzrua@163.com>
2022-03-01 13:29:41 +0800

    submitted by Zerui Liu, 220301

--- debug_simu/gen_simu.py
+++ debug_simu/gen_simu.py
@@ -8,6 +8,57 @@ import multiprocessing as mp
 import gc
 import sys
 
+datadir_time = "/mnt/e/noisetimedata/timeseq/"
+datadir_noise = "/mnt/e/noisetimedata/noisedata_hist/"
+
+storedir = "/mnt/e/noisetimedata/test_gen/"
+
+# number range: range(num_echo*num_batch*num_bthlc, (num_echo+1)*num_bthlc)
+global num_batch
+global num_bthlc
+global num_echo
+
+num_echo = 0
+num_batch = 250
+num_bthlc = 20
+
+
+def generate_random_parameter_set(u0_max=1, max_iter=100):
+    ''' generate a random set of parameters. '''
+    rho = 10.**random.uniform(-4, -2) # log-flat between 1e-4 and 1e-2
+    q = 10.**random.uniform(-4, 0) # including both planetary & binary events
+    s = 10.**random.uniform(np.log10(0.3), np.log10(3))
+    alpha = random.uniform(0, 360) # 0-360 degrees
+    ## use Penny (2014) parameterization for small-q binaries ##
+    if q < 1e-3:
+        if q/(1+q)**2 < (1-s**4)**3/27/s**8: # close topology #
+            if s < 0.1:
+                uc_max = 0
+            else:
+                uc_max = (4+90*s**2)*np.sqrt(q/(1+s**2))/s
+            xc = (s-(1-q)/s)/(1+q)
+            yc = 0.
+        elif s**2 > (1+q**(1/3.))**3/(1+q): # wide topology #
+            uc_max = (4+min(90*s**2, 160/s**2))*np.sqrt(q)
+            xc = s - 1./(1+q)/s
+            yc = 0.
+        else: # resonant topology
+            xc, yc = 0., 0.
+            uc_max = 4.5*q**0.25
+        alpha_rad = alpha/180.*np.pi
+        n_iter = 0
+        for i_3 in range(50):
+            uc = random.uniform(0, uc_max)
+            u0 = uc - xc*np.sin(alpha_rad) + yc*np.cos(alpha_rad)
+            n_iter += 1
+            if u0 < u0_max:
+                break
+            if n_iter > max_iter:
+                break
+    else: # for large-q binaries, use the traditional parameterization
+        u0 = random.uniform(0, u0_max)
+    return (u0, rho, q, s, alpha)
+
 class TimeData(object):
     def __init__(self,datadir,num_point):
         self.time_datadir = datadir
@@ -85,24 +136,36 @@ class NoiseData(object):
 
     
     def gen_index(self,mag,cad=0.1):     
-        if ((mag <= self.mag_range[0]).any())|((mag >= self.mag_range[1]).any()):
-            raise RuntimeError("The magnitude is out of range.")
+        
         return ((np.array(mag)-self.mag_range[0])//cad).astype(np.int)
 
     def get_sigma_0(self,index0,index1):
-        if len(self.err_state[index0])==0:
-            raise RuntimeError("Histbin is empty, Cannot get the error in the histbin.")
         return self.err_state[index0][index1]
 
     def get_sigma(self,index0,index1):
         get_sigma_np = np.frompyfunc(self.get_sigma_0,2,1)
         return get_sigma_np(index0,index1).astype(np.float64)
-
+    '''
     def noisemodel(self,mag):
         posi = self.gen_index(mag).astype(np.int)
         index_sigma_mag = np.floor(np.random.rand(len(mag))*self.state[posi]).astype(np.int)
         gauss_list = np.random.randn(len(mag))
         return (gauss_list)*self.get_sigma(posi,index_sigma_mag),self.get_sigma(posi,index_sigma_mag)
+    '''
+    def noisemodel(self,mag):
+        posi = self.gen_index(mag).astype(np.int)
+        if np.min(posi)<0 or np.max(posi)>=len(self.state):
+            raise RuntimeError("The range of magnitude is out of range")
+        errorbar = []
+        for i_posi in range(len(posi)):
+            histbin_index = np.int(np.random.rand()*self.state[posi[i_posi]])
+            errorbar.append(self.err_state[posi[i_posi]][histbin_index])
+
+        errorbar = np.array(errorbar)
+        gauss_list = np.random.randn(len(mag))
+
+        return gauss_list*errorbar,errorbar
+
     
     def __del__(self):
         del self.datadir 
@@ -118,52 +181,12 @@ def magnitude_tran(magni,m_0=20):
 def chi_square(single,binary,sigma):
     return np.sum(np.power((single-binary)/sigma,2))
 
-def generate_random_parameter_set(u0_max=1, max_iter=100):
-    ''' generate a random set of parameters. '''
-    rho = 10.**random.uniform(-4, -2) # log-flat between 1e-4 and 1e-2
-    q = 10.**random.uniform(-4, 0) # including both planetary & binary events
-    s = 10.**random.uniform(np.log10(0.3), np.log10(3))
-    alpha = random.uniform(0, 360) # 0-360 degrees
-    ## use Penny (2014) parameterization for small-q binaries ##
-    if q < 1e-3:
-        if q/(1+q)**2 < (1-s**4)**3/27/s**8: # close topology #
-            if s < 0.1:
-                uc_max = 0
-            else:
-                uc_max = (4+90*s**2)*np.sqrt(q/(1+s**2))/s
-            xc = (s-(1-q)/s)/(1+q)
-            yc = 0.
-        elif s**2 > (1+q**(1/3.))**3/(1+q): # wide topology #
-            uc_max = (4+min(90*s**2, 160/s**2))*np.sqrt(q)
-            xc = s - 1./(1+q)/s
-            yc = 0.
-        else: # resonant topology
-            xc, yc = 0., 0.
-            uc_max = 4.5*q**0.25
-        alpha_rad = alpha/180.*np.pi
-        n_iter = 0
-        for i_3 in range(50):
-            uc = random.uniform(0, uc_max)
-            u0 = uc - xc*np.sin(alpha_rad) + yc*np.cos(alpha_rad)
-            n_iter += 1
-            if u0 < u0_max:
-                break
-            if n_iter > max_iter:
-                break
-    else: # for large-q binaries, use the traditional parameterization
-        u0 = random.uniform(0, u0_max)
-    return (u0, rho, q, s, alpha)
 
-global num_batch
-global num_bthlc
-
-num_batch = 500
-num_bthlc = 5
 
 def gen_simu_data(index_batch):
     print("Batch",index_batch,"has started")
-    c_time = TimeData(datadir="/mnt/e/noisetimedata/timeseq/",num_point=1000)
-    noi_model = NoiseData(datadir="/mnt/e/noisetimedata/noisedata_hist/")
+    c_time = TimeData(datadir=datadir_time,num_point=1000)
+    noi_model = NoiseData(datadir=datadir_noise)
     counter_total = 0
     for index_slc in range(num_bthlc):
         counter_total = 0
@@ -173,7 +196,8 @@ def gen_simu_data(index_batch):
                 times,d_times = c_time.get_t_seq()
                 break
             except:
-                c_time = TimeData(datadir="/mnt/e/noisetimedata/timeseq/",num_point=1000)
+                c_time = TimeData(datadir=datadir_time,num_point=1000)
+                print("time failure once")
                 continue
         t_E = (times[-1]-times[0])/4
 
@@ -181,23 +205,26 @@ def gen_simu_data(index_batch):
         count_gen_args = 0
         count_args_bearing = 0
         for i_4 in range(50):
-            try:
-                basis_m = np.min([20+2*np.random.randn(),22])
-                u_0, rho, q, s, alpha = generate_random_parameter_set()
-                args_data = [u_0, rho, q, s, alpha, t_E, basis_m, t_0]
-                # single = mm.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E})
-                planet = mm.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': s, 'q': q, 'alpha': alpha,'rho': rho})
-                # planet_degeneracy = mm.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': 1/s, 'q': q, 'alpha': alpha,'rho': rho})
-                
-                planet.set_default_magnification_method('VBBL')
-                
-                lc = planet.magnification(times)
-                
-                
-                noi, sig = noi_model.noisemodel(magnitude_tran(lc,m_0=basis_m))
-                lc_noi = magnitude_tran(lc,basis_m) + noi
+            
+            basis_m = np.min([20+2*np.random.randn(),22])
+            basis_m = np.max([18,basis_m])
+            u_0, rho, q, s, alpha = generate_random_parameter_set()
+            args_data = [u_0, rho, q, s, alpha, t_E, basis_m, t_0]
+            # single = mm.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E})
+            planet = mm.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': s, 'q': q, 'alpha': alpha,'rho': rho})
+            # planet_degeneracy = mm.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': 1/s, 'q': q, 'alpha': alpha,'rho': rho})
+            
+            planet.set_default_magnification_method('VBBL')
+            
+            lc = planet.magnification(times)
+            if np.min(magnitude_tran(lc,m_0=basis_m)) < 9.3:
+                continue
+            
+            noi, sig = noi_model.noisemodel(magnitude_tran(lc,m_0=basis_m))
+            lc_noi = magnitude_tran(lc,basis_m) + noi
 
-                break
+            break
+            '''
             except:
                 print("fail generation of args")
                 count_gen_args += 1
@@ -205,18 +232,18 @@ def gen_simu_data(index_batch):
                     raise RuntimeError("Noise model crash")
                 if count_gen_args > 20:
                     print("Reload noise model: ", count_args_bearing)
-                    noi_model = NoiseData(datadir="/mnt/e/noisetimedata/noisedata_hist/")
+                    noi_model = NoiseData(datadir=datadir_noise)
                     count_gen_args = 0
                     count_args_bearing += 1
                     continue
                 continue
+            '''
     
-        data_array=np.array([args_data,list(times),list(d_times),list(lc_noi),list(sig),list(magnitude_tran(lc,basis_m))])
-        np.save('/mnt/e/noisetimedata/test_gen/'+str(index_batch*num_bthlc+index_slc)+".npy",data_array,allow_pickle=True)
+        data_array=np.array([args_data,list(times),list(d_times),list(lc_noi),list(sig),list(magnitude_tran(lc,basis_m))],dtype=object)
+        np.save(storedir+str(index_batch*num_bthlc+index_slc)+".npy",data_array,allow_pickle=True)
         print("lc "+str(index_batch*num_bthlc+index_slc),datetime.datetime.now())
         
 if __name__=="__main__":
-    num_echo = 0
     u = num_echo*num_batch
     starttime = datetime.datetime.now()
     print("starttime:",starttime)
--- test_noisemodel.py
+++ test_noisemodel.py
@@ -1,20 +1,147 @@
+from datetime import time
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 import random
+from scipy.optimize import curve_fit
+
+def exp_func(x,a,b,c):
+    return b*np.exp(a*x)+c
 
 rootdir = "/mnt/e/noisetimedata/noisedata_hist/"
 rootdir_time = "/mnt/e/noisetimedata/timeseq/"
+targetdir = "/mnt/e/noisetimedata/noisedata_hist_new/"
+figdir = "/home/zerui_liu/work/KMTNet_breaker/testnoisemodelfig/"
 
-# mag_err_data = np.load(rootdir+os.listdir(rootdir)[random.randint(0,24)],allow_pickle=True)
-time_data = np.load(rootdir_time+os.listdir(rootdir_time)[random.randint(0,24)],allow_pickle=True)
+mylog = open('recode.log', mode='w', encoding='utf-8')
 
-# list0 = mag_err_data[0]
-# list1 = mag_err_data[1]
-# 
-# list2 = mag_err_data[2]
-# list3 = mag_err_data[3]
+fileslist = os.listdir(rootdir)
+fileslist_time = os.listdir(rootdir_time)
 
-time_data_index = [x for x in range(len(time_data)) if len(time_data[x]) > 1000]
 
-print(time_data_index)
+def killbadtimedata():
+    for i in range(len(fileslist_time)):
+        timedata = np.load(rootdir_time+fileslist_time[i],allow_pickle=True)
+        print(fileslist_time[i])
+        print(len(timedata))
+        timedata_new = []
+        
+        for j in range(len(timedata)):
+            if len(timedata[j])<10:
+                print("length of timeseq too short",timedata[j],"posi: ",j,file=mylog)
+        
+        '''
+        
+
+        for j in range(len(timedata)):
+            if 
+        '''
+        
+            
+
+def killbaddata():
+    llim = []
+    rlim = []
+    for i in range(len(fileslist)):
+        alert = 0
+
+        mag_err_data = np.load(rootdir+fileslist[i],allow_pickle=True)
+        print(fileslist[i])
+
+        # print(fileslist[i],", (leftlim, rightlim):",mag_err_data[0][0],mag_err_data[0][1],file=mylog)
+        range_message = mag_err_data[0] 
+        hist_message = mag_err_data[1]
+        hist_bins = mag_err_data[2]
+        llim.append(range_message[0])
+        rlim.append(range_message[1])
+        for j in range(len(hist_message)):
+            histbin = np.array(hist_bins[j])
+            
+            if (np.isnan(histbin)).any() or (np.isinf(histbin)).any():
+                print(fileslist[i],"nan/inf alert: ",j,file=mylog)
+            
+            if (histbin<0).any():
+                print(fileslist[i],"negative alert: ",j,file=mylog)
+
+            if (np.isnan(1/histbin)).any():
+                print(fileslist[i],"zero nan alert: ",j,file=mylog)
+            
+            if (np.isinf(1/histbin)).any():
+                print(fileslist[i],"zero inf alert: ",j,file=mylog)
+                
+                alert += 1
+                histbin_dealed = list(histbin[histbin>0])
+                hist_bins[j] = histbin_dealed
+                hist_message[j] = len(histbin_dealed)
+        if alert > 0:
+            data_array = np.array([range_message,hist_message,hist_bins],dtype=object)
+            np.save(targetdir+fileslist[i], data_array, allow_pickle = True)
+    print(np.max(llim))
+    print(np.min(rlim))
+
+def fit(index):
+    mag_err_data = np.load(rootdir+fileslist[index],allow_pickle=True)
+
+    range_message = mag_err_data[0] 
+    hist_message = mag_err_data[1]
+    hist_bins = mag_err_data[2]
+
+    mags = []
+    errs_mean = []
+    errs_std = []
+
+    for i in range(len(hist_message)):
+        if hist_message[i] > 0:
+            errs_mean.append(np.mean(hist_bins[i]))
+            errs_std.append(np.std(hist_bins[i]))
+            mags.append(range_message[0]+(range_message[1]-range_message[0])*i/len(hist_message))
+
+    popt, pcov = curve_fit(exp_func,mags,errs_mean)#,bounds=(left_bound,right_bound))
+    lc_fit = exp_func(np.array(mags),*popt)
+
+    plt.figure()
+    plt.subplot(211)
+    plt.scatter(mags,errs_mean,s=3)
+    plt.plot(mags,lc_fit)
+    plt.xlabel("mag")
+    plt.ylabel("err_mean")
+    plt.subplot(212)
+    plt.scatter(mags,errs_std,s=3)
+    plt.xlabel("mag")
+    plt.ylabel("err_std")
+    plt.savefig(figdir+fileslist[index][:-4]+".png")
+    plt.close()
+
+    return popt
+
+def fix_zero(index,fixnum = 10):
+    mag_err_data = np.load(rootdir+fileslist[index],allow_pickle=True)
+
+    range_message = mag_err_data[0] 
+    hist_message = np.array(mag_err_data[1]).astype(np.int)
+    hist_bins = mag_err_data[2]
+
+    mags = []
+    errs_mean = []
+    errs_std = []
+
+    for i in range(len(hist_message)):
+        if hist_message[i] > 0:
+            errs_mean.append(np.mean(hist_bins[i]))
+            errs_std.append(np.std(hist_bins[i]))
+            mags.append(range_message[0]+(range_message[1]-range_message[0])*i/len(hist_message))
+
+    popt, pcov = curve_fit(exp_func,mags,errs_mean)#,bounds=(left_bound,right_bound))
+
+    for i in range(len(hist_message)):
+        if hist_message[i] <= 0:
+            hist_message[i] = fixnum
+            mags_posi = range_message[0]+(range_message[1]-range_message[0])*i/len(hist_message)
+            hist_bins[i] = list((0.1+0.9*np.random.uniform(size=fixnum))*exp_func(mags_posi,*popt))
+    data_array = np.array([range_message,list(hist_message),hist_bins],dtype=object)
+    np.save(targetdir+fileslist[index], data_array, allow_pickle = True)
+
+
+killbadtimedata()
+
+mylog.close()
